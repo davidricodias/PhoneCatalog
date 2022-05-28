7f0817c94218680792639ac05961856dc9c54806
José David Rico Días <davidricodias.dev@gmail.com>
2022-05-28 14:00:19 +0200

    Added basic docker config for server

--- server/index.js
+++ server/index.js
@@ -19,9 +19,11 @@ database.once('connected', () => {
 // Init app
 const app = express();
 
+const port = process.env.PORT || 3001;
+
 // Allows CORS
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "http://localhost:"+port); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
@@ -29,6 +31,6 @@ app.use(function(req, res, next) {
 app.use(express.json());
 app.use('/phones', routes); // Redirect the phones route to the routes middleware
 
-app.listen(3001, () => {
-    console.log("Server working on port ", 3001)
+app.listen(port, () => {
+    console.log("Server working on port ", port)
 });
